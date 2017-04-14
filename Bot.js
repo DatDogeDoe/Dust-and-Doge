1cb51aea1a21284914816b2e0fd24d5bb1bd01c0
TheDustyBunny <TheDustyBunny@users.noreply.github.com>
2017-04-14 16:29:53 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -22,7 +22,6 @@ if(message.content.startsWith(client.user)){
   CleverBotClient.setNick("Bendy");
   CleverBotClient.create(function (err, session){
   console.log("CreatED/ing CleverBotClient! function> | session: "+ session);
-}
 });
 
 {0
