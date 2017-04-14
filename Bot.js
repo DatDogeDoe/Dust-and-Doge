139f723e2adca6869271a7c38e945c5a168c939f
TheDustyBunny <TheDustyBunny@users.noreply.github.com>
2017-04-14 16:09:47 +0200

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -2,16 +2,18 @@ const Discord = require('discord.js');
 const client = new Discord.Client();
 const config = require('./config.json');
 const adminfile = config.adminfile;
-const BotTokenLol = config.token;
+//removed the constant for the token, if it's only used once, there's no need to use a constant/variable
 const prefix = config.prefix;
 
 
 client.on('ready', () => {
-  console.log('Bot Status: Online');
-  console.log('Starting All Commands');
-  console.log('Saving Latest Changes');
+  console.log('Bot Status: Online\nStarting All Commands\nSaving Latest Changes');//logging multiple lines on one line :O
   client.user.setGame( +client.guilds.size+ ' Servers | ' +client.users.size+ ' Users');
   client.user.setStatus('online');
 });
 
-client.login(BotTokenLol);
+client.on('message', message =>{
+  if(message.author === client.user) return;
+});
+
+client.login(config.token);
