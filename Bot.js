fec5239e2cc6ee1dc954d7071b1e2740ff52903a
DatDogeDoe <DatDogeDoe@users.noreply.github.com>
2017-04-14 07:36:09 -0600

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,1 +1,12 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
 
+
+
+client.on('ready', () => {
+  console.log('Bot Status: Online');
+  console.log('Starting All Commands');
+  console.log('Saving Latest Changes');
+  client.user.setGame( +client.guilds.size+ ' Servers | ' +client.users.size+ ' Users');
+  client.user.setStatus('online');
+});
