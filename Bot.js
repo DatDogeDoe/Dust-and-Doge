1a05c308e4671ae8512e22c525a3124eb333d0ab
DatDogeDoe <DatDogeDoe@users.noreply.github.com>
2017-04-14 07:37:41 -0600

    Update Bot.js

--- Bot.js
+++ Bot.js
@@ -1,6 +1,9 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-
+const config = require('./config.json');
+const adminfile = config.adminfile;
+const BotTokenLol = config.token;
+const prefix = config.prefix;
 
 
 client.on('ready', () => {
@@ -10,3 +13,5 @@ client.on('ready', () => {
   client.user.setGame( +client.guilds.size+ ' Servers | ' +client.users.size+ ' Users');
   client.user.setStatus('online');
 });
+
+client.login(BotTokenLol);
